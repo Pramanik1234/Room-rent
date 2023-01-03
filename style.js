8ff8d5393dca7312863ed33cb830a05129bb26aa
Pramanik1234 <121309897+Pramanik1234@users.noreply.github.com>
2023-01-03 13:22:36 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+let xmark = document.getElementById("xmark");
+let bar = document.getElementById("bar");
+let link = document.getElementById("link");
+bar.addEventListener('click' , function(){
+    link.style.width="200px"
+})
+xmark.addEventListener('click' , function(){
+    link.style.width="0px"
+})
+
